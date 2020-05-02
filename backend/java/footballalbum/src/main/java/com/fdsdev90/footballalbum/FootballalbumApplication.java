9c4ce81b1857c2a9142c25138a240c379dfd88d9
fdsdev90@gmail.com <fdsdev90@gmail.com>
2020-05-02 18:23:36 +0200

    init the project with backend implementation

---
+++ backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/FootballalbumApplication.java
@@ -0,0 +1,13 @@
+package com.fdsdev90.footballalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballalbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballalbumApplication.class, args);
+	}
+
+}
---
+++ backend/java/footballalbum/src/test/java/com/fdsdev90/footballalbum/FootballalbumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdsdev90.footballalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballalbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
