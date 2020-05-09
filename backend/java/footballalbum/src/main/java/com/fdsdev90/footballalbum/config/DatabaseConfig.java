867e679ee93a0691fe615ff332020f7cce451c0a
fdsdev90@gmail.com <fdsdev90@gmail.com>
2020-05-09 19:00:46 +0200

    started implementations

--- backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseConfig.java
+++ backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseConfig.java
@@ -1,4 +1,60 @@
 package com.fdsdev90.footballalbum.config;
 
-public class DatabaseConfig {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig
+{
+
+    @Value("${database.server}")
+    private String databaseServer;
+    @Value("${database.url}")
+    private String jdbcURL;
+    @Value("${database.username}")
+    private String userName;
+    @Value("${database.password}")
+    private String password;
+
+
+    public void setDatabaseServer(String databaseServer)
+    {
+        this.databaseServer = databaseServer;
+    }
+
+    public String getDatabaseServer()
+    {
+        return databaseServer;
+    }
+
+    public void setJdbcURL(String jdbcURL)
+    {
+        this.jdbcURL = jdbcURL;
+    }
+
+    public String getJdbcURL()
+    {
+        return jdbcURL;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
 }
--- backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseStart.java
+++ backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseStart.java
@@ -1,4 +1,26 @@
 package com.fdsdev90.footballalbum.config;
 
-public class DatabaseStart {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseStart
+{
+
+    static
+    {
+        DatabaseConfig databaseConfig = new DatabaseConfig();
+        Connection connection = null;
+
+        try
+        {
+            connection = DriverManager.getConnection(databaseConfig.getJdbcURL(),databaseConfig.getUserName(),databaseConfig.getPassword());
+        }
+        catch(Exception e)
+        {
+
+        }
+
+    }
+
+
 }
