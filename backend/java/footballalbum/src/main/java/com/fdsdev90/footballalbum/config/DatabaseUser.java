4f436eb5dd57dddefa84e93c40726664d3b762af
fdsdev90@gmail.com <fdsdev90@gmail.com>
2020-05-09 20:07:34 +0200

    used dao interface

--- backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseUser.java
+++ backend/java/footballalbum/src/main/java/com/fdsdev90/footballalbum/config/DatabaseUser.java
@@ -1,4 +1,30 @@
 package com.fdsdev90.footballalbum.config;
 
-public class DatabaseUser {
+import com.fdsdev90.footballalbum.dao.FootballPlayerDAOInt;
+import com.fdsdev90.footballalbum.model.FootballPlayerModel;
+
+import java.util.Date;
+import java.util.List;
+
+public class DatabaseUser implements FootballPlayerDAOInt
+{
+    @Override
+    public List<FootballPlayerModel> getPlayersByBirthDate(Date birthDate) {
+        return null;
+    }
+
+    @Override
+    public List<FootballPlayerModel> getPlayersByTeam(String team) {
+        return null;
+    }
+
+    @Override
+    public FootballPlayerModel getPlayerInfoGoals(int id) {
+        return null;
+    }
+
+    @Override
+    public FootballPlayerModel getAverageGoals(int id) {
+        return null;
+    }
 }
